8d150ff89ac996a0fc4e5d6b4316443851ce7e6f
Isaac Keleher <isaac.keleher@gmail.com>
2021-01-07 23:10:06 +1100

    refactor: using reduce func in place of loop

--- bakery.py
+++ bakery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import functools
 """
 this is a program to calculate the prices of baked goods
 """
@@ -10,32 +10,24 @@ VS5 = {3: 6.99, 5: 8.99}
 MB11 = {2: 9.95, 5: 16.95, 8: 24.95}
 CF = {3: 5.95, 5: 9.95, 9: 16.99}
 
-# a way to access using the code
+# a way to access using the item code
 ITEMS = {"VS5": VS5, "MB11": MB11, "CF": CF}
 
-def calculate_price(num, item):
-    # fill the list of possible combos
-    iter_list = [k for k in ITEMS[item]]
-    # now we work out the best combination
-    combos = a(iter_list, num)
-    # grab the smallest number in combo list
-    smallest_combo_len = min(map(lambda x: len(x), combos))
-    # and calculated the smallest (by length) list
-    smallest_combo = 0
-    for c in combos:
-        if len(c) <= smallest_combo_len:
-            smallest_combo = c
-    # then sum up for our prices
-    total_price = 0
-    for s in smallest_combo:
-        total_price += ITEMS[item][s]
-    # print our calculations
-    print(str(num) + " " + str(item) + " " + str(total_price))
-    print(smallest_combo)
+def calculate_price(amount, item):
+
+    iter_list = [k for k in ITEMS[item]] # grab bundle types
+    combos = a(iter_list, amount) # work out the best combination
+    # calculate the smallest (by length) list
+    smallest_combo = functools.reduce(lambda a,b : a if a > b else b,combos)
+
+    # sum the price and print details
+    print(str(amount) + ' ' + str(item) + ' $' + str("{:.2f}".format(sum(ITEMS[item][s] for s in smallest_combo))))
+    
+    for c in set(smallest_combo): # using set here to get only unique integers
+        print(4*' ' +  str(smallest_combo.count(c)) + " x " + str(c) + " $" + str(ITEMS[item][c]))
 
 # modified from:
 # https://stackoverflow.com/questions/20193555/finding-combinations-to-the-provided-sum-value
-
 def a(lst, target):
     # lst = list of combos, target is what we are summing to
     def _a(l, r, t):
@@ -51,8 +43,8 @@ def a(lst, target):
 
 if __name__ == "__main__":
     # input = input()
-    # num, item = input.split()
-    # calculate_price(num, item)
+    # amount, item = input.split()
+    # calculate_price(amount, item)
     # tests
     calculate_price(10, "VS5")
     calculate_price(14, "MB11")
