9f2982966cdcd60d3479a198989c23cc3e8b5d7c
Isaac Keleher <isaac.keleher@gmail.com>
2021-01-09 01:41:24 +1100

    wip: lookup table and sorting

--- bakery.py
+++ bakery.py
@@ -1,64 +1,55 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from functools import reduce
-
 """
 This is a program to calculate the prices of baked goods
 """
-
-# priceing info formatted as bundle:price
 VS5 = {3: 6.99, 5: 8.99}
 MB11 = {2: 9.95, 5: 16.95, 8: 24.95}
 CF = {3: 5.95, 5: 9.95, 9: 16.99}
-# use ITEMS to access data using item code
-ITEMS = {"VS5": VS5, "MB11": MB11, "CF": CF}
+ITEMS = {"VS5": VS5, "MB11": MB11, "CF": CF}  # access data using item code
 
 
 def calculate_price(amount, item):
-    """ 
-    Args:
-        amount (int): total num of individual items in order
-        item (string): item code e.g. "VS5"
-    """
     iter_list = [k for k in ITEMS[item]]  # grab bundle types
-    combos = subsets_with_sum(iter_list, amount)  # work out the best combination
-    # calculate the smallest (by length) list
-    smallest_combo = reduce(lambda a, b: a if a > b else b, combos)
-    # sum the price and print details
-    print(str(amount) + ' ' + str(item) + ' $' +
+    # combos = subsets_with_sum(iter_list, amount)  # find best combination
+    combos = subsets_with_sum(iter_list, amount)
+    # combos = subsetSum(iter_list, amount)
+    # print("FINAL CHOICES: ", combos)
+    smallest_combo = reduce(lambda a, b: a if a >
+                            b else b, combos)  # get smallest list
+    print(str(amount) + ' ' + str(item) + ' $' +  # print price details
           str("{:.2f}".format(sum(ITEMS[item][s] for s in smallest_combo))))
-    # using set here to get only unique integers
-    for c in set(smallest_combo):
+    for c in set(smallest_combo):  # use set to get only unique integers
         print(4*' ' + str(smallest_combo.count(c)) +
               " x " + str(c) + " $" + str(ITEMS[item][c]))
 
 
 def subsets_with_sum(lst, target):
-    """
-    modified from:
-    https://stackoverflow.com/questions/20193555/finding-combinations-to-the-provided-sum-value
-        
-    Args:
-        lst (list): types of "bundles" we can use for shipping
-        target (int): amount of individual items we want in total
-
-    Returns:
-        list: The best combination to reduce shipping costs
-    """
-    def _a(l, r, t):
-        if t == sum(l): r.append(l)
-        elif t < sum(l): return  # uh-oh we went too far!
-        # otherwise recursively keep going
-        for u in range(0, len(lst)):
-            _a(l + [lst[u]], r, t)
-        return r  # return best combo
-    return _a([], [], target)
+    lookup = {} # avoid duplicate combos
+    # Time Complexity: ???
+    def _a(l, r, t, lookup):
+        l.sort() # this really makes it shine
+        if t == sum(l):
+            r.append(l)  # found a solution!
+        elif t < sum(l):
+            return  # uh-oh we went too far!
+        key = tuple(l)
+        # print("KEY:", key)
+        if (key not in lookup):
+            for u in range(0, len(lst)):  # recursively keep going
+                _a(l + [lst[u]], r, t, lookup)
+            lookup[key] = l
+        elif key in lookup:
+            return
+        return r  # return combos
+    return _a([], [], target, lookup)
 
 
 if __name__ == "__main__":
     # input = input()
     # amount, item = input.split()
     # calculate_price(amount, item)
-    calculate_price(10, "VS5")
+    calculate_price(150, "VS5")
     calculate_price(14, "MB11")
     calculate_price(13, "CF")
